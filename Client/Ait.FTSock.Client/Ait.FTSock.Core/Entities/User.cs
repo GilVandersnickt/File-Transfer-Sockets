715de242c2f161efeefcf39dff539f50fdb455b3
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-05 09:25:46 +0100

    Add user entity

---
+++ Client/Ait.FTSock.Client/Ait.FTSock.Core/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Ait.FTSock.Core.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string ActivePath { get; set; }
+    }
+}
