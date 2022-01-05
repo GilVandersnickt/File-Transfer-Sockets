3ab2953b5dbfb851ae54a2a83be30baada43d7d1
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-05 09:06:46 +0100

    Add entity ftfile

---
+++ Server/Ait.FTSock.Server/Ait.FTSock.Core/Entities/FTFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ait.FTSock.Core.Entities
+{
+    public class FTFile
+    {
+        public string Name { get; set; }
+        public string FullPath { get; set; }
+        public byte Size { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
