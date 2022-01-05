be7d0d96e09497134f59f428ddaecb04b2c49c7b
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-05 09:27:35 +0100

    Add constant error messages

---
+++ Client/Ait.FTSock.Client/Ait.FTSock.Core/Constants/ErrorMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace Ait.FTSock.Core.Constants
+{
+    public static class ErrorMessages
+    {
+        public const string ValidationTitleErrorMessage = "Ongeldige invoer";
+        public const string ValidationErrorMessage = "Voer een correct ip adres, poortnummer en gebruikersnaam in";
+        public const string ServerUnreachableErrorMessage = "Server onbereikbaar";
+    }
+}
