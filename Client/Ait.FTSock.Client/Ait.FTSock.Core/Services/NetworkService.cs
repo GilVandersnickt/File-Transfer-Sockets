8c6e4925dd8a5acc56abf3625d79343d86913f09
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-05 09:28:51 +0100

    Add network service

---
+++ Client/Ait.FTSock.Client/Ait.FTSock.Core/Services/NetworkService.cs
@@ -0,0 +1,18 @@
+﻿using Ait.FTSock.Core.Constants;
+using System.Collections.Generic;
+
+namespace Ait.FTSock.Core.Services
+{
+    public class NetworkService
+    {
+        public List<int> GetPorts()
+        {
+            List<int> ports = new List<int>();
+            for (int i = PortValues.MinValue; i < (PortValues.MaxValue + 1); i++)
+            {
+                ports.Add(i);
+            }
+            return ports;
+        }
+    }
+}
