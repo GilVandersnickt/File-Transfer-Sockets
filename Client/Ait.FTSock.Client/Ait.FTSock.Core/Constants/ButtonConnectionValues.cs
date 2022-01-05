a6053cc362dd11de53b0d3a25aa0b76e5622108c
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-05 09:27:03 +0100

    Add constant button connection values

---
+++ Client/Ait.FTSock.Client/Ait.FTSock.Core/Constants/ButtonConnectionValues.cs
@@ -0,0 +1,8 @@
+﻿namespace Ait.FTSock.Core.Constants
+{
+    public class ButtonConnectionValues
+    {
+        public const string Connect = "Connect";
+        public const string Disconnect = "Disconnect";
+    }
+}
