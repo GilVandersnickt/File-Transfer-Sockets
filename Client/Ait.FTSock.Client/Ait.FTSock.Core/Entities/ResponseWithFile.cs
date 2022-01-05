491e983ced6c0b5c1998194aaabd268032d151d1
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-05 09:26:24 +0100

    Add response with file entity

---
+++ Client/Ait.FTSock.Client/Ait.FTSock.Core/Entities/ResponseWithFile.cs
@@ -0,0 +1,8 @@
+﻿namespace Ait.FTSock.Core.Entities
+{
+    public class ResponseWithFile : Response
+    {
+        public string FileName { get; set; }
+        public byte[] FileBytes { get; set; }
+    }
+}
