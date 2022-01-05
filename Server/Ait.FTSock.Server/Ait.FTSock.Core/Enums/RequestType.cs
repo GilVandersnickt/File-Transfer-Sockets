08f1fe8fb7dc9ad6fd4a911c9860eeaad7519d73
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-05 09:07:33 +0100

    Add enum request type

---
+++ Server/Ait.FTSock.Server/Ait.FTSock.Core/Enums/RequestType.cs
@@ -0,0 +1,13 @@
+﻿namespace Ait.FTSock.Core.Enums
+{
+    public enum RequestType
+    {
+        CONNECT,
+        CLOSE,
+        CDUP,
+        CDDIR,
+        MKDIR,
+        GET,
+        PUT
+    }
+}
