6eab0d65067ee786576b226625ce9fcffd7ad361
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-05 09:07:07 +0100

    Add entity response

---
+++ Server/Ait.FTSock.Server/Ait.FTSock.Core/Entities/Response.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ait.FTSock.Core.Entities
+{
+    public class Response
+    {
+        public string Username { get; set; }
+        public string ActivePath { get; set; }
+        public Guid Id { get; set; }
+        public List<FTFolder> SubFolders { get; set; }
+        public List<FTFile> Files { get; set; }
+    }
+}
