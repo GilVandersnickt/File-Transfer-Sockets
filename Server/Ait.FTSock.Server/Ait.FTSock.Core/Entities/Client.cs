f4fbbd5afa26f693339df2fc53bb88f972048eaa
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-05 09:06:23 +0100

    Add entity client

---
+++ Server/Ait.FTSock.Server/Ait.FTSock.Core/Entities/Client.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Ait.FTSock.Core.Entities
+{
+    public class Client
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string ActivePath { get; set; }
+    }
+}
