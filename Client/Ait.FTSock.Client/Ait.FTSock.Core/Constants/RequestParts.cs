1904402ee7d1327deca905969f4025e6148b406d
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-05 09:27:26 +0100

    Add constant request parts

---
+++ Client/Ait.FTSock.Client/Ait.FTSock.Core/Constants/RequestParts.cs
@@ -0,0 +1,8 @@
+﻿namespace Ait.FTSock.Core.Constants
+{
+    public class RequestParts
+    {
+        public const string Delimiter = "|*|";
+        public const string EndOfMessage = "##EOM";
+    }
+}
