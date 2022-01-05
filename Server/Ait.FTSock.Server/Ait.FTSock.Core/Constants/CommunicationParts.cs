87b88325dd35f898e61ca2a4f7dc2568bda264a2
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-05 09:08:00 +0100

    Add constants communication parts

---
+++ Server/Ait.FTSock.Server/Ait.FTSock.Core/Constants/CommunicationParts.cs
@@ -0,0 +1,9 @@
+﻿namespace Ait.FTSock.Core.Constants
+{
+    public class CommunicationParts
+    {
+        public const string Error = "ERROR";
+        public const string Delimiter = "|*|";
+        public const string EndOfMessage = "##EOM";
+    }
+}
