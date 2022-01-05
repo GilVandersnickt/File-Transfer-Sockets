d4e34fade02330d980c918e08e1aa33d8e986de0
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-05 09:30:34 +0100

    Add create folder window

---
+++ Client/Ait.FTSock.Client/Ait.FTSock.Client/CreateFolderWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace Ait.FTSock.Client
+{
+    /// <summary>
+    /// Interaction logic for CreateFolderWindow.xaml
+    /// </summary>
+    public partial class CreateFolderWindow : Window
+    {
+        public string FolderName { get; private set; }
+        public CreateFolderWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            FolderName = txtFolderName.Text;
+            Close();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            FolderName = string.Empty;
+            Close();
+        }
+    }
+}
