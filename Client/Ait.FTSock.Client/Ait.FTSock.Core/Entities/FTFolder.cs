097e611629e54455e5e85ad835e66e807b3981a7
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-05 09:26:06 +0100

    Add ft folder entity

---
+++ Client/Ait.FTSock.Client/Ait.FTSock.Core/Entities/FTFolder.cs
@@ -0,0 +1,13 @@
+﻿namespace Ait.FTSock.Core.Entities
+{
+    public class FTFolder
+    {
+        public string Name { get; set; }
+        public string FullPath { get; set; }
+        public string Parent { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
