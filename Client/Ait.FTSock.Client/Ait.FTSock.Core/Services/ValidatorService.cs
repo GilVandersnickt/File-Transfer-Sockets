68b2677012383f0365a99a33cf2abe7ee79075ec
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-05 09:28:39 +0100

    Add validator service

---
+++ Client/Ait.FTSock.Client/Ait.FTSock.Core/Services/ValidatorService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Ait.FTSock.Core.Services
+{
+    public class ValidatorService
+    {
+        public bool Validate(string ipAddress, object port)
+        {
+            return ValidateIpAddress(ipAddress) && ValidatePort(port);
+        }
+
+        private static bool ValidateIpAddress(string ipAddress)
+        {
+            if (String.IsNullOrWhiteSpace(ipAddress))
+                return false;
+
+            string[] splitAddresses = ipAddress.Split('.');
+            if (splitAddresses.Length != 4)
+                return false;
+
+            return splitAddresses.All(splitAddress => byte.TryParse(splitAddress, out byte result));
+        }
+        private static bool ValidatePort(object port)
+        {
+            return port != null;
+        }
+    }
+}
